6ef232a5969eb209ec37090c4ee4bb9c84f08d8f
aymeric maille <aymeric.maille@laposte.net>
2021-10-19 11:40:36 +0200

    adding field new and create, prep for mapbox API

--- app/controllers/fields_controller.rb
+++ app/controllers/fields_controller.rb
@@ -8,9 +8,19 @@ class FieldsController < ApplicationController
     @field = Field.find(params[:id])
   end
 
-  # private
+  def new
+    @field = Field.new
+  end
 
-  # def field_params
-  #   params.require(:field).permit(:field_id)
-  # end
+  def create
+    @field = Field.create!(field_params)
+    Owning.create!(field: @field, user: current_user)
+    redirect_to fields_path
+  end
+
+  private
+
+  def field_params
+    params.require(:field).permit(:field_id, :name)
+  end
 end

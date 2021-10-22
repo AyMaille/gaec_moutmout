0ad9052db1966575a06aaadf4b29a45fbfe71c33
aymeric maille <aymeric.maille@laposte.net>
2021-10-22 11:25:11 +0200

    changing style on mapbox

--- app/javascript/components/map.js
+++ app/javascript/components/map.js
@@ -4,7 +4,7 @@ import mapboxgl from 'mapbox-gl'
 mapboxgl.accessToken = 'pk.eyJ1IjoiYXltZXJpYzIzIiwiYSI6ImNrcnlvcmZvMTBreHUydm1yc2w4ZGN4MnQifQ.nWPJUbTLTBJwO7Y6TchQnw';
   const map = new mapboxgl.Map({
     container: 'map', // container ID
-    style: 'mapbox://styles/mapbox/streets-v11', // style URL
+    style: 'mapbox://styles/mapbox/satellite-v9', // style URL
     center: [0.7737501347029947, 44.55994903365295], // starting position [lng, lat]
     zoom: 16, // starting zoom
   });

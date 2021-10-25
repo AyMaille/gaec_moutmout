b89cf2fe4ed7bb5adee1736c60efa67aa051d145
aymeric maille <aymeric.maille@laposte.net>
2021-10-25 11:28:34 +0200

    adding drawing area feature

--- app/javascript/components/map.js
+++ app/javascript/components/map.js
@@ -1,12 +1,45 @@
 import mapboxgl from 'mapbox-gl'
+import MapboxDraw from "@mapbox/mapbox-gl-draw";
 // import 'mapbox-gl/dist/mapbox-gl.css';
 
 mapboxgl.accessToken = 'pk.eyJ1IjoiYXltZXJpYzIzIiwiYSI6ImNrcnlvcmZvMTBreHUydm1yc2w4ZGN4MnQifQ.nWPJUbTLTBJwO7Y6TchQnw';
-  const map = new mapboxgl.Map({
-    container: 'map', // container ID
-    style: 'mapbox://styles/mapbox/satellite-v9', // style URL
-    center: [0.7737501347029947, 44.55994903365295], // starting position [lng, lat]
-    zoom: 16, // starting zoom
-  });
+const map = new mapboxgl.Map({
+  container: 'map', // container ID
+  style: 'mapbox://styles/mapbox/satellite-v9', // style URL
+  center: [0.7737501347029947, 44.55994903365295], // starting position [lng, lat]
+  zoom: 15, // starting zoom
+});
 
-export { map }
+const draw = new MapboxDraw({
+  displayControlsDefault: false,
+  // Select which mapbox-gl-draw control buttons to add to the map.
+  controls: {
+    polygon: true,
+    trash: true
+  },
+  // Set mapbox-gl-draw to draw by default.
+  // The user does not have to click the polygon control button first.
+  defaultMode: 'draw_polygon'
+});
+map.addControl(draw);
+
+map.on('draw.create', updateArea);
+map.on('draw.delete', updateArea);
+map.on('draw.update', updateArea);
+
+function updateArea(e) {
+  const data = draw.getAll();
+  const answer = document.getElementById('calculated-area');
+  if (data.features.length > 0) {
+    const area = turf.area(data);
+    // Restrict the area to 2 decimal points.
+    const rounded_area = Math.round(area * 100) / 100;
+    answer.innerHTML = `<p><strong>${rounded_area}</strong></p><p>square meters</p>`;
+  } else {
+    answer.innerHTML = '';
+    if (e.type !== 'draw.delete')
+      alert('Click the map to draw a polygon.');
+  }
+}
+
+export { map, draw, updateArea }
---
+++ outTurf.js
@@ -0,0 +1,8 @@
+Hash: 08da7c6b27376c9f0c27
+Version: webpack 4.46.0
+Time: 52ms
+Built at: 10/25/2021 9:40:53 AM
+        Asset      Size  Chunks             Chunk Names
+manifest.json  23 bytes          [emitted]  
+
+ERROR in Entry module not found: Error: Can't resolve 'application.js' in '/home/aymeric/code/AyMaille/gaec_moutmout'

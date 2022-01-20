b8bfcaadd1ca3309311b580217c8e8437c1d0d13
aymeric maille <aymeric.maille@laposte.net>
2022-01-20 12:45:09 +0100

    changing data type of comments_content

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_05_100913) do
+ActiveRecord::Schema.define(version: 2022_01_20_113928) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,7 +28,7 @@ ActiveRecord::Schema.define(version: 2022_01_05_100913) do
   create_table "comments", force: :cascade do |t|
     t.string "name"
     t.string "type"
-    t.string "content"
+    t.text "content"
     t.bigint "sheep_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
